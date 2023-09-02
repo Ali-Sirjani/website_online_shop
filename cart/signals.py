86eb4c5e229313afee95d91511204169753c58c7
Ali Sirjani <alisirjani1234@gmail.com>
2023-09-02 14:21:34 +0330

    Refactor: Improved Cart Class Initialization and Enhanced Code Clarity
    This commit represents a significant enhancement to the `Cart` class, focusing on initialization and code clarity. The key improvements include:
    
    1. Initialization Refinement:
    During initialization, the code has been optimized to remove inactive items (with remove_inactive_items method) from the cart, ensuring that only active items are retained. This refinement enhances the reliability and integrity of the cart.
    
    2. Code Clarity:
    Variable names have been refined to improve code readability, making it easier for developers to understand and work with the codebase.
    
    3. New Method:
    `remove_inactive_items`: A new method, `remove_inactive_items`, has been added to remove inactive items from the cart during initialization, further enhancing cart management and efficiency.
    
    4. Update Quantity Logic:
    The `update_quantity` method has been updated to handle negative quantity values correctly, primarily for the "remove" action. This correction ensures consistent behavior when reducing the quantity of items in the cart.
    
    These improvements collectively result in a more robust `Cart` class that simplifies cart management and maintenance within the application. The changes enhance both the functionality and code quality of the `Cart` class, contributing to a more reliable and efficient codebase.
    
    refactor: Improved Cart Handling for User Login
    
    This commit enhances the cart handling mechanism for user login. The key improvements include:
    
    1. Signal Handling Improvement:
    The `add_item_to_cart_from_session` signal handler has been updated to use the `@receiver` decorator, which enhances code clarity and follows best practices for signal handling.
    
    2. Active Product Check:
    Before adding items to the cart, the signal handler now checks if the products are active. Inactive products are skipped, ensuring only active products are added to the cart.
    
    These enhancements result in a more reliable and efficient cart management system when users log in. The code is now more concise and better handles scenarios where users add products to their cart before logging in and then have those products correctly associated with their account after login.
    
    feat: Introduce Signal for Associating Users with Orders
    
    This commit introduces a new signal handler in the cart app's signals.py module. The key changes include:
    
    1. Signal Function:
    A new signal handler, `set_user_for_order_with_customer_null`, has been added. This handler is triggered upon user login (user_logged_in).
    
    2. Order Linking:
    The signal handler identifies orders with null customers that match the user's email and completed status. It then links these orders to the logged-in user, ensuring proper ownership.
    
    3. Cleanup of Uncompleted Orders:
    Additionally, uncompleted orders with null customers that match the user's email are removed to maintain data consistency.
    
    By implementing this signal handler, the codebase gains an essential feature to automatically link users to their relevant orders and maintain clean data by removing uncompleted orders with null customers when users log in.

--- cart/signals.py
+++ cart/signals.py
@@ -1,35 +1,47 @@
 from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
-
-import requests
-import json
 
 from .cart import Cart
 from .models import Order, OrderItem
 
 
+@receiver(user_logged_in)
 def add_item_to_cart_from_session(sender, user, request, **kwargs):
     if request.session.get('cart'):
 
         cart = Cart(request)
         order, create = Order.objects.get_or_create(customer=user, completed=False)
         for item_session in cart:
-            item, create = OrderItem.objects.get_or_create(
-                product=item_session['product'],
-                order=order,
-            )
+            product = item_session.get('product')
 
-            if create:
-                item.quantity = item_session['quantity']
-                item.save()
+            if product.active:
+                item, create = OrderItem.objects.get_or_create(
+                    product=item_session['product'],
+                    order=order,
+                )
 
-            else:
-                item.quantity += item_session['quantity']
-                item.save()
+                if create:
+                    item.quantity = item_session['quantity']
+                    item.save()
+
+                else:
+                    item.quantity += item_session['quantity']
+                    item.save()
 
         messages.success(request, _('The product add to your cart'))
         cart.clear_cart()
 
 
-user_logged_in.connect(add_item_to_cart_from_session)
+@receiver(user_logged_in)
+def set_user_for_order_with_customer_null(sender, user, request, **kwargs):
+    orders_with_user_email_completed = Order.objects.filter(customer=None, email=user.email, completed=True)
+
+    if orders_with_user_email_completed.exists():
+        orders_with_user_email_completed.update(customer=user)
+
+    orders_with_user_email_uncompleted = Order.objects.filter(customer=None, email=user.email, completed=False)
+
+    if orders_with_user_email_uncompleted.exists():
+        orders_with_user_email_uncompleted.delete()
