9296302917ab28cc30b4564618f042c8ba32b4e4
Ali Sirjani <alisirjani1234@gmail.com>
2023-09-02 16:27:32 +0330

    refactor: Improved User Checkout Logic and Form Handling
    In this commit, the `check_out_user_login` function has undergone enhancements to improve user checkout logic and form handling. The key improvements include:
    
    1. Shipping Address Handling:
    Shipping address creation and management have been optimized by passing an instance of `ShippingAddress` to `ShippingAddressForm` using the `get_or_create` method, enhancing code efficiency.
    
    2. Transaction ID Management:
    Transaction IDs are now generated more effectively, ensuring that a new ID is only created when the `order.transaction` field is empty. This improvement enhances order transaction reliability.
    
    3. Error Handling:
    Improved error handling ensures that if the total does not match the order's cart total, an error message is added to the request, providing better feedback to users.
    
    4. Form Validation Scope:
    The condition for validating the shipping address form (`form_shipping.is_valid()`) is now within the scope of the total validation condition (`total == order.get_cart_total`). This change ensures that the form is only validated when the total is correct, enhancing the overall user experience.
    
    These enhancements collectively result in a more efficient and user-friendly user checkout process, contributing to a better overall user experience.
    
    refactor: Optimized Anonymous User Checkout Process and Enhanced Error Handling
    
    This commit represents a significant overhaul of the anonymous user checkout process, resulting in cleaner, more efficient code and enhanced error handling:
    
    1. Refactored Checkout Logic:
    The `check_out_user_anonymous` function has been optimized by eliminating the need for the `save_items_and_shipping_address_user_anonymous` function, making the code cleaner and more maintainable.
    
    2. Cart Item Validation:
    Before proceeding with checkout, the code checks if the cart is empty. If no items are in the cart, it adds an informative message to the request and redirects the user to the product list to encourage item selection.
    
    3. Simplified Order Retrieval:
    To determine if an order already exists for the given email, the code has been improved to use the `get_or_create` method, eliminating the need for complex try-except blocks.
    
    4. Consistent Total Validation:
    The code ensures that the total provided in the checkout form matches the actual total value of items in the cart. If they don't match, a clear error message is added to the request, allowing users to address any discrepancies.
    
    These enhancements collectively result in a more straightforward and user-friendly anonymous user checkout process. Additionally, the codebase benefits from robust error handling, providing users with precise feedback when issues arise during checkout. This commit serves as a significant step towards an improved user experience and maintainable codebase.

--- cart/utils.py
+++ cart/utils.py
@@ -10,30 +10,32 @@ from .models import Order, OrderItem, ShippingAddress
 def check_out_user_login(request):
     customer = request.user
     order, created = Order.objects.get_or_create(customer=customer, completed=False)
+
     order.del_in_act_items()
     if order.get_cart_items == 0:
         messages.info(request, _('The Your cart is empty! Please first add some product in your cart.'))
         return redirect('products:products_list')
 
+    shipping, create = ShippingAddress.objects.get_or_create(customer=customer, order=order)
+
     form_order = OrderForm(request.POST or None, instance=order)
-    form_shipping = ShippingAddressForm(request.POST or None)
+    form_shipping = ShippingAddressForm(request.POST or None, instance=shipping)
+
     if form_order.is_valid():
-        form_order_obj = form_order.save(commit=False)
-        form_order_obj.customer = customer
-        transaction_id = datetime.datetime.now().timestamp()
-        order.transaction = transaction_id
+
+        if not order.transaction:
+            transaction_id = datetime.datetime.now().timestamp()
+            form_order.instance.transaction = transaction_id
 
         # for test total
         total = None
         try:
-            total = int(form_order.cleaned_data['total'])
+            total = int(form_order.cleaned_data.get('total'))
         except ValueError:
             messages.error(request, _('You change the data of checkout form!'))
 
         if total == order.get_cart_total:
-            # save total price
-            form_order_obj.order_price = total
-            form_order_obj.save()
+            form_order.save()
 
             # save product price
             for item in order.act_items():
@@ -42,122 +44,83 @@ def check_out_user_login(request):
 
             order.backup_total()
 
-            print('this is order in login: ', order.get_cart_total_profit_pass)
+            if form_shipping.is_valid():
+                if order.shipping:
+                    form_shipping.save()
+                    return redirect('payment:sandbox_process')
 
-        if form_shipping.is_valid():
-            if order and order.shipping:
-                form_shipping = form_shipping.save(commit=False)
-                form_shipping.customer = customer
-                form_shipping.order = order
-                form_shipping.save()
-                return redirect('payment:sandbox_process')
+        else:
+            messages.info(request, _('Something went wrong! Please try again.'))
 
     items = order.act_items().order_by('-datetime_added')
 
     return form_order, form_shipping, order, items
 
 
-def save_items_and_shipping_address_user_anonymous(request, cart, order_obj, total, form_shipping):
-    order_obj.order_price = total
-
-    for item in cart:
-        OrderItem.objects.get_or_create(
-            product=item['product'],
-            order=order_obj,
-            quantity=item['quantity'],
-            item_price=item['product'].price,
-        )
-    # save product price
-    for item in order_obj.act_items():
-        item.save_price()
-        item.backup_product()
-
-    order_obj.backup_total()
-
-    if form_shipping.is_valid():
-        if order_obj.shipping:
-            try:
-                shipping = ShippingAddress.objects.get(order=order_obj)
-                form_shipping = ShippingAddressForm(request.POST, instance=shipping)
-                if form_shipping.is_valid():
-                    form_shipping.save()
-            except ShippingAddress.DoesNotExist:
-                form_shipping_obj = form_shipping.save(commit=False)
-                form_shipping_obj.order = order_obj
-                form_shipping_obj.save()
-
-            return True
-
-    return False
-
-
 def check_out_user_anonymous(request, cart):
+    if cart.get_cart_items() == 0:
+        messages.info(request, _('The Your cart is empty! Pleas first add some product in your cart.'))
+        return redirect('products:products_list')
+
     form_order = OrderForm(request.POST or None)
     form_shipping = ShippingAddressForm(request.POST or None)
     if form_order.is_valid():
-        email_user = form_order.cleaned_data['email']
+        email_user = form_order.cleaned_data.get('email')
+
+        order, create = Order.objects.get_or_create(customer=None, email=email_user, completed=False)
+
+        request.session['order_pk'] = order.pk
+
+        orders_obj_pk = request.session.get('orders_pk_list')
+        if not orders_obj_pk:
+            orders_obj_pk = []
+
+        if not (order.pk in orders_obj_pk):
+            orders_obj_pk.append(order.pk)
+            request.session['orders_pk_list'] = orders_obj_pk
+
+        total = None
         try:
-            order_obj = Order.objects.get(customer=None, email=email_user, completed=False)
-            request.session['order_pk'] = order_obj.pk
+            total = int(form_order.cleaned_data.get('total'))
+        except ValueError:
+            messages.error(request, _('You change the data of checkout form!'))
 
-            orders_obj_pk = request.session.get('orders_pk_list')
-            if not orders_obj_pk:
-                orders_obj_pk = []
+        if total == cart.get_cart_total:
+            form_order = OrderForm(request.POST, instance=order)
 
-            if not (order_obj.pk in orders_obj_pk):
-                orders_obj_pk.append(order_obj.pk)
-                request.session['orders_pk_list'] = orders_obj_pk
+            if not order.transaction:
+                transaction_id = datetime.datetime.now().timestamp()
+                form_order.instance.transaction = transaction_id
 
-            print('this is request.session: ', request.session['orders_pk_list'])
-            form_order = OrderForm(request.POST, instance=order_obj)
+            form_order.save()
 
-            if form_order.is_valid():
+            # delete order items for update in save_items_and_shipping_address_user_anonymous()
+            order.items.all().delete()
 
-                total = None
-                try:
-                    total = int(form_order.cleaned_data['total'])
-                except ValueError:
-                    messages.error(request, _('You change the data of checkout form!'))
+            for item in cart:
+                OrderItem.objects.create(
+                    product=item['product'],
+                    order=order,
+                    quantity=item['quantity'],
+                    item_price=item['product'].price,
+                )
+            # save product price
+            for item in order.act_items():
+                item.save_price()
+                item.backup_product()
 
-                if total == cart.get_cart_total:
-                    form_order.save()
-                    # delete order items for update in save_items_and_shipping_address_user_anonymous()
-                    order_obj.items.all().delete()
+            order.backup_total()
 
-                    result = save_items_and_shipping_address_user_anonymous(request, cart, order_obj, total,
-                                                                            form_shipping)
-                    if result:
-                        return redirect('payment:sandbox_process')
+            if form_shipping.is_valid():
+                if order.shipping:
+                    shipping, create = ShippingAddress.objects.get_or_create(order=order)
 
-        except Order.DoesNotExist:
-            form_order_obj = form_order.save(commit=False)
-            transaction_id = datetime.datetime.now().timestamp()
-            form_order_obj.transaction = transaction_id
+                    form_shipping = ShippingAddressForm(request.POST, instance=shipping)
+                    form_shipping.save()
 
-            total = None
-            try:
-                total = int(form_order.cleaned_data['total'])
-            except ValueError:
-                messages.error(request, _('You change the data of checkout form!'))
-
-            if total == cart.get_cart_total:
-                form_order_obj.save()
-
-                order_obj = Order.objects.get(customer=None, email=email_user, completed=False)
-                request.session['order_pk'] = order_obj.pk
-
-                orders_obj_pk = request.session.get('orders_pk_list')
-                if not orders_obj_pk:
-                    orders_obj_pk = []
-
-                if not (order_obj.pk in orders_obj_pk):
-                    orders_obj_pk.append(order_obj.pk)
-                    request.session['orders_pk_list'] = orders_obj_pk
-                    # request.session.modified = True
-                print('this is request.session: ', request.session['orders_pk_list'])
-
-                result = save_items_and_shipping_address_user_anonymous(request, cart, order_obj, total, form_shipping)
-                if result:
                     return redirect('payment:sandbox_process')
+
+        else:
+            messages.info(request, _('Something went wrong! Please try again.'))
 
     return form_order, form_shipping
