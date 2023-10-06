87dcf7ba4974bc54d86005fc76a92d95028127df
Ali Sirjani <alisirjani1234@gmail.com>
2023-10-07 01:56:35 +0330

    refactor: Enhanced 'SetUsername' Model Admin Configuration
    This commit improves the admin configuration for the 'SetUsername' model by specifying the fields displayed, enabling inline editing for the 'first_time' field in the list view, and making the 'user' field read-only when editing an existing instance.
    
    These enhancements enhance the usability and clarity of the 'SetUsername' model within the Django admin interface.
    
    refactor: Improved 'Profile' Model Admin Configuration
    
    This commit enhances the admin configuration for the 'Profile' model by specifying the displayed fields, enabling autocomplete for the 'user' field, ordering profiles by their creation date in descending order, and making the 'user' field read-only when editing an existing instance. Additionally, it introduces two custom methods, 'full_name' and 'num_of_user_orders,' to provide a better admin experience, displaying the full name of users and linking to their orders in the list view along with showing the count of their orders.
    
    These improvements improve the usability and management of user profiles within the Django admin interface.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,47 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.shortcuts import reverse
+from django.utils.http import urlencode
 
 from .models import SetUsername, Profile
 
 
 @admin.register(SetUsername)
 class SetUsernameAdmin(admin.ModelAdmin):
-    pass
+    fields = ('user', 'first_time')
+    list_display = ('user', 'first_time')
+    list_editable = ('first_time', )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = []
+        if obj:
+            readonly_fields.append('user')
+
+        return readonly_fields
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    fields = ('user', 'location', 'first_name', 'last_name', 'phone', 'picture',
+              'datetime_created', 'datetime_modified')
+    list_display = ('user', 'full_name', 'num_of_user_orders')
+    autocomplete_fields = ('user',)
+    ordering = ('-datetime_created', )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = ['datetime_created', 'datetime_modified', ]
+        if obj:
+            readonly_fields.append('user')
+
+        return readonly_fields
+
+    def full_name(self, obj):
+        name = f'{obj.first_name} {obj.last_name}'
+        if name and not name.isspace():
+            return name
+
+        return None
+
+    def num_of_user_orders(self, obj):
+        url = (reverse('admin:cart_order_changelist') + '?' + urlencode({'customer_id': obj.user.pk}))
+        return format_html('<a href="{}">{}</a>', url, obj.user.order.count())
