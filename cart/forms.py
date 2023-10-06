986f5c6444f00863f2b940462d85b5562754a7bc
Ali Sirjani <alisirjani1234@gmail.com>
2023-10-07 02:47:33 +0330

    feat: Added 'OrderAdminForm' for Future Use in Model Admin
    This commit introduces the 'OrderAdminForm' class, a custom form designed for use in the model admin of the 'Order' model. The form contains logic to ensure data consistency and validity when handling orders. It includes the following features:
    
    - If an order is marked as incomplete ('completed' is False), the form checks for existing incomplete orders for the same user and raises an error if found, preventing duplicate incomplete orders.
    
    - For completed orders, the form verifies the presence of a shipping address and raises an error if one is missing. This ensures that all completed orders have a valid shipping address.
    
    This form will be utilized in the model admin for the 'Order' model to enhance data validation and user experience when managing orders.
    
    feat: Introduced 'OrderItemAdminForm' for Future Model Admin Use
    
    This commit introduces the 'OrderItemAdminForm' class, a custom form tailored for potential use in the model admin of the 'OrderItem' model. This form includes validation logic to maintain data integrity:
    
    - The form checks if the associated order is marked as 'completed.' If the order is completed, it raises an error to prevent modifications to completed orders, ensuring data integrity.
    
    The 'OrderItemAdminForm' will be available for future use in the model admin to ensure consistent and valid data management for order items.

--- cart/forms.py
+++ cart/forms.py
@@ -1,6 +1,50 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
-from .models import Order, ShippingAddress
+from .models import Order, OrderItem, ShippingAddress
+
+
+class OrderAdminForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def clean(self):
+        clean_data = super().clean()
+
+        if not clean_data.get('completed'):
+            try:
+                user = clean_data.get('customer')
+
+                order = Order.objects.get(customer=user, completed=False)
+                order_pk = order.pk
+                self.add_error('customer', _(f'There is an incomplete order with pk {order_pk} for user {user}'))
+
+            except Order.DoesNotExist:
+                pass
+
+        elif self.instance:
+            try:
+                self.instance.address
+            except ShippingAddress.DoesNotExist:
+                self.add_error(None, _('Complete order must have a shipping address'))
+
+        return clean_data
+
+
+class OrderItemAdminForm(forms.ModelForm):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+    def clean(self):
+        clean_data = super().clean()
+        order = clean_data.get('order')
+
+        if order.completed:
+            self.add_error('order', _('This order is complete'))
+
+        return clean_data
 
 
 class OrderForm(forms.ModelForm):
