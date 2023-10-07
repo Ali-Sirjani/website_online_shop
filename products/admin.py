522fff03d92644e759e0c1e7ba654ce6e391bbb7
Ali Sirjani <alisirjani1234@gmail.com>
2023-10-07 03:39:09 +0330

    enhancement: Improved 'Category' Model Admin
    This commit enhances the 'Category' model admin for a more comprehensive and user-friendly experience. Key improvements include:
    
    - List Display: The list display now includes 'name_en,' 'name_fa,' and 'products_count,' providing valuable information at a glance.
    
    - Search Functionality: Administrators can now search for categories based on their English and Persian names, simplifying the process of locating specific categories.
    
    - 'products_count' Function: A custom 'products_count' function has been added to display the number of products within each category. This feature improves the admin's ability to assess category popularity and content.
    
    These enhancements enhance the 'Category' model admin's usability and reporting capabilities, making it easier to manage and analyze product categories.
    
    refactor: Added 'autocomplete_fields' to 'ProductCommentTabu' TabularInline
    
    This commit enhances the 'ProductCommentTabu' TabularInline by adding the 'autocomplete_fields' attribute for the 'author' field. This improvement provides a more user-friendly and efficient experience when selecting authors for product comments, as it enables autocomplete functionality.
    
    feat: Enhanced 'ProductAdmin' Model Admin Configuration
    
    This commit enhances the 'ProductAdmin' model admin configuration to improve the user experience when managing products. The following improvements have been made:
    
    - Added search functionality for the 'title' field, allowing easier product searches.
    
    - Enabled autocomplete functionality for the 'category' field, streamlining the process of selecting product categories.
    
    These enhancements provide more efficient and user-friendly product management within the admin interface.
    
    feat: Added 'ProductCommentAdmin' Model Admin
    
    This commit introduces the 'ProductCommentAdmin' model admin configuration, enhancing the management of product comments within the admin interface. Key features of this configuration include:
    
    - Display of essential fields like 'product,' 'star,' 'confirmation,' and 'datetime_modified' in the list view, improving the overview of product comments.
    
    - Autocomplete functionality for 'product' and 'author' fields to streamline the selection of associated products and authors.
    
    - Dynamic 'readonly_fields' based on the presence of an object, ensuring certain fields remain unmodifiable when editing existing comments.
    
    - Ordering of product comments by 'datetime_modified' in ascending order for better chronological presentation.
    
    The addition of this model admin improves the usability and efficiency of managing product comments in the admin panel.

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.db import models
+from django.shortcuts import reverse
 from django.forms import Textarea, NumberInput
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 from jalali_date.admin import ModelAdminJalaliMixin
 from modeltranslation.admin import TranslationAdmin
@@ -9,28 +12,54 @@ from .models import Product, ProductComment, Category
 from .forms import ProductFormAdmin
 
 
+@admin.register(Category)
+class CategoryAdmin(TranslationAdmin):
+    fields = ('name_en', 'name_fa', 'slug_change', 'slug_en', 'slug_fa')
+    list_display = ('name_en', 'name_fa', 'products_count',)
+    search_fields = ('name_en', 'name_fa',)
+
+    def products_count(self, obj):
+        url = (reverse('admin:products_product_changelist') + '?' + urlencode({'category': obj.pk}))
+        return format_html('<a href="{}">{}</a>', url, obj.products.count())
+
+
 class ProductCommentTabu(admin.TabularInline):
     model = ProductComment
     readonly_fields = ('datetime_modified', )
     fields = ('author', 'text', 'confirmation', 'datetime_modified')
     ordering = ('-datetime_modified', )
     extra = 1
+    autocomplete_fields = ('author', )
     formfield_overrides = {
-        models.TextField: {'widget': Textarea(attrs={'cols': 70, 'row': 5})}
+        models.TextField: {'widget': Textarea(attrs={'cols': 70, 'rows': 4})}
     }
-
-
-@admin.register(Category)
-class CategoryAdmin(TranslationAdmin):
-    fields = ('name_en', 'name_fa', 'slug_change', 'slug_en', 'slug_fa')
 
 
 @admin.register(Product)
 class ProductAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    form = ProductFormAdmin
     list_display = ('title', 'price', 'datetime_created', 'datetime_modified', 'active')
     ordering = ('-datetime_modified', )
-    form = ProductFormAdmin
+    search_fields = ('title', )
+    autocomplete_fields = ('category', )
     inlines = (ProductCommentTabu, )
     formfield_overrides = {
         models.PositiveIntegerField: {'widget': NumberInput()}
     }
+
+
+@admin.register(ProductComment)
+class ProductCommentAdmin(admin.ModelAdmin):
+    fields = ('product', 'author', 'text', 'star', 'edited', 'confirmation',
+              'datetime_created', 'datetime_modified',)
+
+    list_display = ('product', 'star', 'confirmation', 'datetime_modified',)
+    ordering = ('datetime_modified',)
+    autocomplete_fields = ('product', 'author')
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = ['datetime_created', 'datetime_modified',]
+        if obj:
+            readonly_fields.extend(['product', 'author'])
+
+        return readonly_fields
